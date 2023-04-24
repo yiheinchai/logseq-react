7603d177fdfab5dd2a53b86deeda7a3d179af5ea
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2023-04-24 12:40:06 +0100

    use custom hooks for TimeInput

--- src/main/frontend/components/datetime.tsx
+++ src/main/frontend/components/datetime.tsx
@@ -28,51 +28,92 @@ const defaultTimestampValue: Timestamp = {
   repeater: {},
 };
 
-const TimeInput = ({ defaultValue }: { defaultValue: string }) => {
-  const [show, setShow] = useState(false);
+interface TimeInputProps {
+  defaultValue: string;
+}
+
+const TIME_INPUT_CLASS = "form-input w-20 ms:w-60";
+const TIME_FORMAT = "HH:mm";
+
+const useShowInput = (initialValue: boolean) => {
+  const [show, setShow] = useState(initialValue);
+
+  const handleShow = useCallback(() => {
+    setShow(true);
+  }, []);
+
+  const handleHide = useCallback(() => {
+    setShow(false);
+  }, []);
+
+  return [show, handleShow, handleHide] as const;
+};
+
+const useDefaultTime = () => {
   const [timestamp, setTimestamp] = useRecoilState(state.timestamp);
+
+  const handleSetTime = useCallback((value: string) => {
+    setTimestamp((prev) => ({ ...prev, time: value }));
+  }, []);
+
+  const handleClearTime = useCallback(() => {
+    setTimestamp((prev) => ({ ...prev, time: null }));
+  }, []);
+
+  const handleSetDefaultTime = useCallback(() => {
+    const { hour, minute } = date.getLocalDate();
+    const result = DateTime.fromObject({ hour, minute }).toFormat(TIME_FORMAT);
+    handleSetTime(result);
+  }, []);
+
+  return [
+    timestamp.time,
+    handleSetTime,
+    handleClearTime,
+    handleSetDefaultTime,
+  ] as const;
+};
+
+const TimeInput: React.FC<TimeInputProps> = React.memo(({ defaultValue }) => {
+  const [show, handleShow, handleHide] = useShowInput(false);
+  const [time, handleSetTime, handleClearTime, handleSetDefaultTime] =
+    useDefaultTime();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     const value = event.target.value;
-    setTimestamp((prev) => ({ ...prev, time: value }));
+    handleSetTime(value);
   };
 
   const handleClick = () => {
-    setShow(false);
-    setTimestamp((prev) => ({ ...prev, time: null }));
+    handleHide();
+    handleClearTime();
   };
 
-  if (show || !string.blank(defaultValue)) {
-    return (
-      <div className="flex flex-row" style={{ height: 32 }}>
-        <input
-          id="time"
-          className="form-input w-20 ms:w-60"
-          defaultValue={defaultValue}
-          onChange={handleChange}
-        />
-        <a className="ml-2 self-center" onClick={handleClick}>
-          {svg.close}
-        </a>
-      </div>
-    );
-  } else {
-    return (
-      <a
-        className="text-sm"
-        onClick={() => {
-          setShow(true);
-          const { hour, minute } = date.getLocalDate();
-          const result = `${hour}:${util.zeroPad(minute)}`;
-          setTimestamp((prev) => ({ ...prev, time: result }));
-        }}
-      >
-        Add time
+  return show || !util.isBlank(defaultValue) ? (
+    <div className="flex flex-row" style={{ height: 32 }}>
+      <input
+        id="time"
+        className={TIME_INPUT_CLASS}
+        defaultValue={defaultValue}
+        onChange={handleChange}
+      />
+      <a className="ml-2 self-center" onClick={handleClick}>
+        {svg.close}
       </a>
-    );
-  }
-};
+    </div>
+  ) : (
+    <a
+      className="text-sm"
+      onClick={() => {
+        handleShow();
+        handleSetDefaultTime();
+      }}
+    >
+      Add time
+    </a>
+  );
+});
 
 const RepeaterCp = ({
   num,
@@ -146,121 +187,120 @@ const RepeaterCp = ({
         }}
       >
         Add repeater
-        </a>
+      </a>
     );
   }
 };
 
 const clearTimestamp = () => {
-    state.setTimestamp(defaultTimestampValue);
-    state.setShowTime(false);
-    state.setShowRepeater(false);
-    state.setState("date-picker/date", null);
-  };
-  
-  const onSubmit = (e?: React.SyntheticEvent) => {
-    if (e) e.preventDefault();
-    const { repeater, ...timestamp } = state.getTimestamp();
-    const date = state.getState("date-picker/date") || t.today();
-    const timestamp = { ...timestamp, date: date };
-    const kind = repeater.duration === "w" ? "++" : ".+";
-    const timestamp = { ...timestamp, repeater: { ...repeater, kind } };
-    const text = repeated.timestampMapToText(timestamp);
-    const blockData = state.getTimestampBlock();
-    const { block, typ, show } = blockData;
-    const editingBlockId = state.getEditBlock().block.uuid;
-    const blockId = block?.block.uuid || editingBlockId;
-    const typ = commands.currentCommand || typ;
-    if (state.editing() && editingBlockId === blockId) {
-      editorHandler.setEditingBlockTimestamp(typ, text);
-    } else {
-      editorHandler.setBlockTimestamp(blockId, typ, text);
-    }
-    if (show) {
-      show.set(false);
-    }
-    clearTimestamp();
-    state.setTimestampBlock(null);
-    commands.restoreState();
-  };
-  
-  const TimeRepeater = () => {
-    const { time, repeater } = useRecoilValue(state.timestamp);
-  
-    return (
-      <div id="time-repeater" className="py-1 px-4">
-        <p className="text-sm opacity-50 font-medium mt-4">Time:</p>
-        <TimeInput defaultValue={time} />
-  
-        <p className="text-sm opacity-50 font-medium mt-4">Repeater:</p>
-        <RepeaterCp {...repeater} />
-  
-        <p className="mt-4">
-          {ui.button("Submit", {
-            onClick: onSubmit,
-          })}
-        </p>
-      </div>
-    );
-  };
-  
-  interface DatePickerProps {
-    domId: string;
-    format: string;
-    ts?: Timestamp;
+  state.setTimestamp(defaultTimestampValue);
+  state.setShowTime(false);
+  state.setShowRepeater(false);
+  state.setState("date-picker/date", null);
+};
+
+const onSubmit = (e?: React.SyntheticEvent) => {
+  if (e) e.preventDefault();
+  const { repeater, ...timestamp } = state.getTimestamp();
+  const date = state.getState("date-picker/date") || t.today();
+  const timestamp = { ...timestamp, date: date };
+  const kind = repeater.duration === "w" ? "++" : ".+";
+  const timestamp = { ...timestamp, repeater: { ...repeater, kind } };
+  const text = repeated.timestampMapToText(timestamp);
+  const blockData = state.getTimestampBlock();
+  const { block, typ, show } = blockData;
+  const editingBlockId = state.getEditBlock().block.uuid;
+  const blockId = block?.block.uuid || editingBlockId;
+  const typ = commands.currentCommand || typ;
+  if (state.editing() && editingBlockId === blockId) {
+    editorHandler.setEditingBlockTimestamp(typ, text);
+  } else {
+    editorHandler.setBlockTimestamp(blockId, typ, text);
   }
-  
-  const DatePicker: React.FC<DatePickerProps> = ({ domId, format, ts }) => {
-    const currentCommand = useRecoilValue(commands.currentCommand);
-    const deadlineOrSchedule =
-      currentCommand &&
-      ["deadline", "scheduled"].includes(currentCommand.toLowerCase());
-    const [date, setDate] = useRecoilState(state.datePickerDate);
-  
-    React.useEffect(() => {
-      clearTimestamp();
-      if (ts) {
-        state.setTimestamp(ts);
-      } else {
-        state.setTimestamp({ time: "", repeater: {} });
-      }
-      if (!date) {
-        setDate(ts?.date || t.today());
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-  
-    return (
-      <div
-        id="date-time-picker"
-        className="flex flex-row"
-        onClick={(e) => util.stop(e)}
-        onMouseDown={(e) => e.stopPropagation()}
-      >
-        {ui.datepicker(date, {
-          deadlineOrSchedule,
-          onChange: (e: React.SyntheticEvent, date: DateTime) => {
-            util.stop(e);
-            const date = t.toDefaultTimeZone(date);
-            const journal = date.journalName();
-            // deadline-or-schedule? is handled in on-submit, not here
-            if (!deadlineOrSchedule) {
-              // similar to page reference
-              editorHandler.insertCommand(
-                domId,
-                pageRef.toPageRef(journal),
-                format,
-                { command: "page-ref" }
-              );
-              state.clearEditorAction();
-              commands.setCurrentCommand(null);
-            }
-          },
+  if (show) {
+    show.set(false);
+  }
+  clearTimestamp();
+  state.setTimestampBlock(null);
+  commands.restoreState();
+};
+
+const TimeRepeater = () => {
+  const { time, repeater } = useRecoilValue(state.timestamp);
+
+  return (
+    <div id="time-repeater" className="py-1 px-4">
+      <p className="text-sm opacity-50 font-medium mt-4">Time:</p>
+      <TimeInput defaultValue={time} />
+
+      <p className="text-sm opacity-50 font-medium mt-4">Repeater:</p>
+      <RepeaterCp {...repeater} />
+
+      <p className="mt-4">
+        {ui.button("Submit", {
+          onClick: onSubmit,
         })}
-        {deadlineOrSchedule && <TimeRepeater />}
-      </div>
-    );
-  };
-  
-  export default DatePicker;
-  
+      </p>
+    </div>
+  );
+};
+
+interface DatePickerProps {
+  domId: string;
+  format: string;
+  ts?: Timestamp;
+}
+
+const DatePicker: React.FC<DatePickerProps> = ({ domId, format, ts }) => {
+  const currentCommand = useRecoilValue(commands.currentCommand);
+  const deadlineOrSchedule =
+    currentCommand &&
+    ["deadline", "scheduled"].includes(currentCommand.toLowerCase());
+  const [date, setDate] = useRecoilState(state.datePickerDate);
+
+  React.useEffect(() => {
+    clearTimestamp();
+    if (ts) {
+      state.setTimestamp(ts);
+    } else {
+      state.setTimestamp({ time: "", repeater: {} });
+    }
+    if (!date) {
+      setDate(ts?.date || t.today());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div
+      id="date-time-picker"
+      className="flex flex-row"
+      onClick={(e) => util.stop(e)}
+      onMouseDown={(e) => e.stopPropagation()}
+    >
+      {ui.datepicker(date, {
+        deadlineOrSchedule,
+        onChange: (e: React.SyntheticEvent, date: DateTime) => {
+          util.stop(e);
+          const date = t.toDefaultTimeZone(date);
+          const journal = date.journalName();
+          // deadline-or-schedule? is handled in on-submit, not here
+          if (!deadlineOrSchedule) {
+            // similar to page reference
+            editorHandler.insertCommand(
+              domId,
+              pageRef.toPageRef(journal),
+              format,
+              { command: "page-ref" }
+            );
+            state.clearEditorAction();
+            commands.setCurrentCommand(null);
+          }
+        },
+      })}
+      {deadlineOrSchedule && <TimeRepeater />}
+    </div>
+  );
+};
+
+export default DatePicker;
